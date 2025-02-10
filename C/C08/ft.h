d6e4a1d95d2add48269c9432069af76e02c14c99
Hyeokju Kwon <hyeokwon@c3r1s1.42gyeongsan.kr>
2025-02-10 14:34:34 +0900

    C08

---
+++ C/C08/ft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Hyeokwon <khj6330865@naver.com>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/10 14:32:29 by Hyeokwon          #+#    #+#             */
+/*   Updated: 2025/02/10 14:32:38 by Hyeokwon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+
+#endif
